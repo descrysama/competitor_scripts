8b14cd430749322c8fade01a4c2542dcfe53079b
descrysama <erwing2002@gmail.com>
2023-05-23 15:04:22 +0200

    ok

--- assets/checkPriceMargin.py
+++ assets/checkPriceMargin.py
@@ -5,10 +5,10 @@ def checkPriceMargin(lowest_price, final_output):
         print(str(key).strip())
         if str(key).strip() in final_output:
             price_ttc = float(final_output[key].replace(',', '.')) * 1.2
-            lowest_competitor_price = float(lowest_price[key])
+            lowest_competitor_price = float(lowest_price[str(key).strip()])
             potential_new_price = lowest_competitor_price - 1
             if potential_new_price >= (price_ttc * 1.16) :
-                final_checked_prices.append([key, potential_new_price])
+                final_checked_prices.append([str(key).strip(), potential_new_price])
             else:
-                final_checked_prices.append([key, price_ttc * 1.15])
+                final_checked_prices.append([str(key).strip(), price_ttc * 1.15])
     return final_checked_prices
